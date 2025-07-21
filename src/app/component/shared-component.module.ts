ed64eabc76a77b065de27f7f5a9affb065a2908c
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-21 20:57:05 -0300

    feat: criação do componente modal

--- src/app/component/shared-component.module.ts
+++ src/app/component/shared-component.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 import { ContainerComponent } from './container/container.component';
 import { InformacoesResumoComponent } from './informacoes-resumo/informacoes-resumo.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [MenuLateralComponent, ContainerComponent, InformacoesResumoComponent],
+  declarations: [MenuLateralComponent, ContainerComponent, InformacoesResumoComponent, ModalComponent],
   imports: [CommonModule,
 ],
-  exports: [MenuLateralComponent,ContainerComponent],
+  exports: [MenuLateralComponent,ContainerComponent,InformacoesResumoComponent,ModalComponent],
 })
 export class SharedComponentModule {}
