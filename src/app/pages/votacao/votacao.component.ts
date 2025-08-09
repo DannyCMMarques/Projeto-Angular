cdfb1535d65a4edb22de449c6714065410ab6de9
Danielly Marques <danielly.marques@dbserver.com.br>
2025-08-09 19:37:22 -0300

    criação da pagina de votação

---
+++ src/app/pages/votacao/votacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-votacao',
+  templateUrl: './votacao.component.html',
+  styleUrls: ['./votacao.component.css']
+})
+export class VotacaoComponent {
+
+}
